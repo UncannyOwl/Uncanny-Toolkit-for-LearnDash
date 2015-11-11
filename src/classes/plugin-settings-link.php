18baa9288fd16b47efd46e5a696ec9f707e70f33
Big-Erny <ernest@uncannycloud.com>
2015-11-11 12:21:26 +0000

    plugin-settings-link.php deleted online with Bitbucket

--- src/classes/plugin-settings-link.php
+++
@@ -1,6 +0,0 @@
-add_filter("plugin_action_links_".plugin_basename(__FILE__), "duplicate_post_plugin_actionsx", 10, 4);
-
-function duplicate_post_plugin_actionsx( $actions, $plugin_file, $plugin_data, $context ) {
-array_unshift($actions, "<a href=\"".menu_page_url('uo-menu-slug', false)."\">".__("Settings")."</a>");
-return $actions;
-}
