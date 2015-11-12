272e536bcdc07dfe5b85e33decd8f60f2af9895d
Ernest Smuga <info@afiadesign.com>
2015-11-12 10:40:03 -0500

    Move plugin page links filter to admin-menu.php and define some handy constants

--- uncanny-learndash-public.php
+++ uncanny-learndash-public.php
@@ -13,9 +13,22 @@ Domain Path: /languages
 // All Class instance are store in Global Variable $uncanny_learndash_public
 global $uncanny_learndash_public;
 
+
 if ( ! isset( $uncanny_learndash_public ) ) {
 	$uncanny_learndash_public = new \stdClass();
 }
+
+if (!defined('ULP_PLUGIN_BASENAME'))
+	define('ULP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+
+if (!defined('ULP_PLUGIN_NAME'))
+	define('ULP_PLUGIN_NAME', trim( ULP_PLUGIN_BASENAME ), '/' );
+
+if (!defined('ULP_PLUGIN_DIR'))
+	define('ULP_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . MYPLUGIN_PLUGIN_NAME);
+
+if (!defined('ULP_PLUGIN_URL'))
+	define('ULP_PLUGIN_URL', WP_PLUGIN_URL . '/' . MYPLUGIN_PLUGIN_NAME);
 
 // Plugins Configurations File
 include_once( dirname( __FILE__ ). '/src/config.php');
@@ -23,17 +36,3 @@ include_once( dirname( __FILE__ ). '/src/config.php');
 // Load all plugin classes(functionality)
 include_once( dirname( __FILE__ ). '/src/boot.php');
 $uncanny_learndash_public = \uncanny_learndash_public\Boot::get_instance();
-
-// Add a simple settings link to our page from the plugins list
-add_filter('plugin_action_links_'.plugin_basename( __FILE__ ), 'link_to_plugins_page', 10, 4);
-
-
-/**
- * @param Array $actions		Plugin action links.
- *
- * @return Array
- */
-function link_to_plugins_page( $actions ) {
-	array_unshift($actions, '<a href="'.menu_page_url('uo-menu-slug', false).'">'.__( 'Settings', \uncanny_learndash_public\Config::get_text_domain() ).'</a>');
-	return $actions;
-}
