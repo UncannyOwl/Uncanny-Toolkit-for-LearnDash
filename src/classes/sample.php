68d28f703d9d74f8149c9c841c9d9b02e24c2120
Ernest Smuga <info@afiadesign.com>
2016-02-09 07:30:42 -0500

    Update sample with settings

--- src/classes/sample.php
+++ src/classes/sample.php
@@ -30,17 +30,17 @@ class Sample extends Config implements RequiredFunctions{
 	 */
 	public static function get_details() {
 
-		$class_title = __( 'Sample Title', Config::get_text_domain() );
+		$class_title = __( 'Sample Title', self::get_text_domain() );
 
 		/* Sample Simple Description */
-		$class_description = __( 'Sample Description A', Config::get_text_domain() );
+		$class_description = __( 'Sample Description A', self::get_text_domain() );
 
 		/* Sample Simple Description with shortcode */
 		$class_description = __( '
 				<p>Sample Description B</p>
 				<p><strong>[sample-shortcode]</strong></p>
 				',
-			Config::get_text_domain() );
+			self::get_text_domain() );
 
 		/* Icon as text - max four characters wil fit */
 		$class_icon = '<span class="uo_icon_text">[/ ]</span>'; // Shortcode
@@ -51,10 +51,14 @@ class Sample extends Config implements RequiredFunctions{
 		/* Icon as img */
 		//icons have variable widths and hieght
 		$icon_styles = 'width: 40px;  padding-top: 5px; padding-left: 9px;';
-		$class_icon = '<img style="'. $icon_styles .'" src="'. Config::get_admin_media('gravity-forms-icon.png') .'" />';
+		$class_icon = '<img style="'. $icon_styles .'" src="'. self::get_admin_media('gravity-forms-icon.png') .'" />';
 
 
-		return array( 'title' => $class_title, 'description' => $class_description, 'dependants_exist' => self::dependants_exist(), 'icon' => $class_icon );
+		return array( 	'title' => $class_title,
+						'description' => $class_description,
+						'dependants_exist' => self::dependants_exist(),
+						'settings' => false, // OR 'settings' => self::get_class_settings( $class_title ),
+						'icon' => $class_icon );
 
 	}
 
@@ -79,6 +83,46 @@ class Sample extends Config implements RequiredFunctions{
 
 		// Return true if no dependency or dependency is available
 		return true;
+	}
+
+	/**
+	* HTML for modal to create settings
+	*
+	* @return boolean || string Return either false or settings html modal
+	*
+	*/
+	public static function get_class_settings( $class_title ){
+
+		// Create options
+		$options = array(
+
+				'checkbox' => array(
+						'label' => 'Settings A',
+						'option_name' => 'a'
+				),
+
+				'radio' => array(
+						'label' => 'Settings Gender',
+						'input_name' => 'gender',
+						'value' => array('Male','Female','Other')
+				),
+
+				'select' => array(
+						'label' => 'Settings Car',
+						'select_name' => 'car',
+						'value' => array('Volvo','Saab','Mercedes','Audi')
+				)
+
+		);
+
+
+		// Build html
+		$html = self::settings_output(array(
+				'class' => __CLASS__,
+				'title' => $class_title,
+				'options' => $options
+		));
+		return $html;
 	}
 
 }
