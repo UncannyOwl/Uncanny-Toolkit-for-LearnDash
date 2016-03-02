12822c1ac204e1b81aa3c52d0092e8c4ef2737d1
Ernest Smuga <info@afiadesign.com>
2016-03-02 13:10:42 -0500

    Initial Addon Push: Hide Admin Bar LearnDash Resume Login Redirect

---
+++ src/classes/hide-admin-bar.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace uncanny_learndash_public;
+
+if( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class HideAdminBar extends Config implements RequiredFunctions{
+
+
+	/**
+	 * class constructor
+	 *
+	 */
+	public function __construct() {
+
+		if( true === self::dependants_exist()){
+			/* Hide admin bar on frontend for the user role */
+			add_filter('show_admin_bar', array( __CLASS__, 'show_admin_bar') );
+		}
+
+	}
+
+	/**
+	 * Description of class in Admin View
+	 *
+	 * @return Array
+	 */
+	public static function get_details() {
+
+		$class_title = __( 'Hide Admin Bar', self::get_text_domain() );
+
+		/* Sample Simple Description */
+		$class_description = __( 'Hide the Admin bar for set of user defined in settings', self::get_text_domain() );
+
+		/* Icon as wp dashicon */
+		$class_icon = '<span class="uo_icon_dashicon dashicons dashicons-admin-settings"></span>';
+
+
+		return array( 	'title' => $class_title,
+						'description' => $class_description,
+						'dependants_exist' => self::dependants_exist(),
+						'settings' => self::get_class_settings( $class_title ),
+						'icon' => $class_icon );
+
+	}
+
+	/**
+	 * Does the plugin rely on another function or plugin
+	 *
+	 * @return boolean || string Return either true or name of function or plugin
+	 *
+	 */
+	public static function dependants_exist(){
+		// Return true if no dependency or dependency is available
+		return true;
+	}
+
+	/**
+	* HTML for modal to create settings
+	*
+	* @return boolean || string Return either false or settings html modal
+	*
+	*/
+	public static function get_class_settings( $class_title ){
+
+		global $wp_roles;
+
+		if ( ! isset( $wp_roles ) ) {
+			$wp_roles = new WP_Roles();
+			$roles = $wp_roles->get_names();
+		}else{
+			$roles = $wp_roles->get_names();
+		}
+
+
+		$options = array();
+
+		foreach ($roles as $role_value => $role_name) {
+			$option_name = 'role['.$role_value.'}';
+			array_push( $options, array( 'type' => 'checkbox', 'label' => $role_name, 'option_name' => $role_value ) );
+		}
+
+		// Build html
+		$html = self::settings_output(array(
+				'class' => __CLASS__,
+				'title' => $class_title,
+				'options' => $options
+		));
+		return $html;
+	}
+
+	/**
+	 * Hide admin bar on frontend for the user role
+	 *
+	 * @return boolean
+	 *
+	 */
+	public static function show_admin_bar(){
+
+		if( is_user_logged_in() ){
+
+			global $current_user;
+
+			$user_roles = $current_user->roles;
+			$user_role = array_shift($user_roles);
+
+			$hide_roles = get_option('HideAdminBar');
+
+			foreach($hide_roles as $role ){
+
+				if( $user_role === $role['name'] &&  $role['value'] === 'on' ){
+					return false;
+				}
+
+			}
+			return true;
+
+		}
+
+		return false;
+	}
+
+}
---
+++ src/classes/login-redirect.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace uncanny_learndash_public;
+
+if( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class LoginRedirect extends Config implements RequiredFunctions{
+
+
+	/**
+	 * class constructor
+	 *
+	 */
+	public function __construct() {
+
+		if( true === self::dependants_exist()){
+
+			/* ADD FILTERS ACTIONS FUNCTION */
+			add_filter( 'login_redirect', array( __CLASS__, 'my_login_redirect'), 10, 3 );
+		}
+
+	}
+
+	/**
+	 * Description of class in Admin View
+	 *
+	 * @return Array
+	 */
+	public static function get_details() {
+
+		$class_title = __( 'Login Redirect', self::get_text_domain() );
+
+		/* Sample Simple Description */
+		$class_description = __( 'Redirect non-admins after login', self::get_text_domain() );
+
+		/* Icon as wp dashicon */
+		$class_icon = '<span class="uo_icon_dashicon dashicons dashicons-arrow-right-alt"></span>';
+
+
+		return array( 	'title' => $class_title,
+						'description' => $class_description,
+						'dependants_exist' => self::dependants_exist(),
+						'settings' => self::get_class_settings( $class_title ),
+						'icon' => $class_icon );
+
+	}
+
+	/**
+	 * Does the plugin rely on another function or plugin
+	 *
+	 * @return boolean || string Return either true or name of function or plugin
+	 *
+	 */
+	public static function dependants_exist(){
+		// Return true if no dependency or dependency is available
+		return true;
+	}
+
+	/**
+	* HTML for modal to create settings
+	*
+	* @return boolean || string Return either false or settings html modal
+	*
+	*/
+	public static function get_class_settings( $class_title ){
+
+		// Get pages to populate drop down
+		$args = array(
+				'sort_order' => 'asc',
+				'sort_column' => 'post_title',
+				'post_type' => 'page',
+				'post_status' => 'publish'
+		);
+
+		$pages = get_pages($args);
+		$drop_down = array( ['value' => 0, 'text' => '- Select Page -'] );
+
+		foreach( $pages as $page ){
+			array_push( $drop_down, array( 'value' => $page->ID, 'text' => $page->post_title ) );
+		}
+
+		// Create options
+		$options = array(
+
+				array(
+						'type' => 'select',
+						'label' => 'Login Page',
+						'select_name' => 'login_page',
+						'value' => $drop_down
+				)
+
+		);
+
+
+		// Build html
+		$html = self::settings_output(array(
+				'class' => __CLASS__,
+				'title' => $class_title,
+				'options' => $options
+		));
+		return $html;
+	}
+
+	/**
+	 * Redirect user after successful login.
+	 *
+	 * @param string $redirect_to URL to redirect to.
+	 * @param string $request URL the user is coming from.
+	 * @param object $user Logged user's data.
+	 * @return string
+	 */
+	public static function my_login_redirect( $redirect_to, $request, $user ) {
+
+		$custom_redirect  = get_permalink( self::get_login_redirect_page_id() );
+
+		//is there a user to check?
+		global $user;
+
+		if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+			return $custom_redirect;
+			//check for admins
+			if ( in_array( 'administrator', $user->roles ) ) {
+				// redirect them to the default place
+				return $redirect_to;
+			} else {
+				return $custom_redirect;
+			}
+		} else {
+			return $redirect_to;
+		}
+	}
+
+	/**
+	 * Set wp-login redirect to frontend page
+	 */
+	private static function get_login_redirect_page_id(){
+
+		$page_id = 0;
+
+		$settings = get_option('LoginRedirect');
+
+		if( false !== $settings){
+
+			foreach( $settings as $setting){
+				if( 'login_page' === $setting['name'] ){
+					$page_id = $setting['value'];
+				}
+
+			}
+
+		}
+
+		return (int)$page_id;
+	}
+
+}
