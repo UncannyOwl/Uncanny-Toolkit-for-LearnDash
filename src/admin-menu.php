c4ff71d23e02a7836b6b9565fd62d8e8c179ce8e
Ernest Smuga <info@afiadesign.com>
2016-03-03 14:45:24 -0500

    Update Feature dependency message

--- src/admin-menu.php
+++ src/admin-menu.php
@@ -228,7 +228,7 @@ class AdminMenu extends Boot {
 
 					<?php
 					if( TRUE !== $dependants_exist ){
-					 echo '<div>You are missing <strong>'. $dependants_exist .'</strong></div>';
+					 echo '<div><strong>'. $dependants_exist .'</strong> is needed for this add-on</div>';
 					}else{
 						?>
 						<div class="uo_feature_button_toggle"></div>
