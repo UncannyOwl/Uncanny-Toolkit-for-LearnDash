8cf0e0d99537d3128326d96051d01e95b34fe0e0
Paul Bearne <pbearne@gmail.com>
2016-03-04 09:16:14 -0500

    white-space

--- uncanny-learndash-public.php
+++ uncanny-learndash-public.php
@@ -18,21 +18,25 @@ if ( ! isset( $uncanny_learndash_public ) ) {
 	$uncanny_learndash_public = new \stdClass();
 }
 
-if (!defined('ULP_PLUGIN_BASENAME'))
-	define('ULP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+if ( ! defined( 'ULP_PLUGIN_BASENAME' ) ) {
+	define( 'ULP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+}
 
-if (!defined('ULP_PLUGIN_NAME'))
-	define('ULP_PLUGIN_NAME', trim( ULP_PLUGIN_BASENAME ), '/' );
+if ( ! defined( 'ULP_PLUGIN_NAME' ) ) {
+	define( 'ULP_PLUGIN_NAME', trim( ULP_PLUGIN_BASENAME ), '/' );
+}
 
-if (!defined('ULP_PLUGIN_DIR'))
-	define('ULP_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . ULP_PLUGIN_NAME);
+if ( ! defined( 'ULP_PLUGIN_DIR' ) ) {
+	define( 'ULP_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . ULP_PLUGIN_NAME );
+}
 
-if (!defined('ULP_PLUGIN_URL'))
-	define('ULP_PLUGIN_URL', WP_PLUGIN_URL . '/' . ULP_PLUGIN_NAME);
+if ( ! defined( 'ULP_PLUGIN_URL' ) ) {
+	define( 'ULP_PLUGIN_URL', WP_PLUGIN_URL . '/' . ULP_PLUGIN_NAME );
+}
 
 // Plugins Configurations File
-include_once( dirname( __FILE__ ). '/src/config.php');
+include_once( dirname( __FILE__ ) . '/src/config.php' );
 
 // Load all plugin classes(functionality)
-include_once( dirname( __FILE__ ). '/src/boot.php');
+include_once( dirname( __FILE__ ) . '/src/boot.php' );
 $uncanny_learndash_public = \uncanny_learndash_public\Boot::get_instance();
