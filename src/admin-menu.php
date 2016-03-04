d25fbd5025518cdc26f875601ddf9a88cbf6c766
Paul Bearne <pbearne@gmail.com>
2016-03-04 10:53:55 -0500

    whitespace strings not translated

--- src/admin-menu.php
+++ src/admin-menu.php
@@ -30,17 +30,17 @@ class AdminMenu extends Boot {
 	public static function register_options_menu_page() {
 
 		$page_title = __( 'UO Page Title', Config::get_text_domain() );
-		$menu_title =  __( 'UO Addons', Config::get_text_domain() );
+		$menu_title = __( 'UO Addons', Config::get_text_domain() );
 		$capability = 'manage_options';
 		$menu_slug = 'uo-menu-slug';
 		$function = array( __CLASS__, 'options_menu_page_output' );
 
 		// Menu Icon blends into sidebar when the default admin color scheme is used
-		$admin_color_scheme =  get_user_meta( get_current_user_id(), 'admin_color', true );
-		if( $admin_color_scheme === 'fresh' ){
-			$icon_url = Config::get_admin_media('menu-icon-light.png');
-		}else{
-			$icon_url = Config::get_admin_media('menu-icon.png');
+		$admin_color_scheme = get_user_meta( get_current_user_id(), 'admin_color', true );
+		if ( 'fresh' === $admin_color_scheme ) {
+			$icon_url = Config::get_admin_media( 'menu-icon-light.png' );
+		} else {
+			$icon_url = Config::get_admin_media( 'menu-icon.png' );
 		}
 
 		$position = 81; // 81 - Above Settings Menu
@@ -56,8 +56,9 @@ class AdminMenu extends Boot {
 	}
 
 	/**
- * @return array
-*/public static function get_available_classes(){
+	 * @return array
+	*/
+	public static function get_available_classes() {
 		// loop file in classes folded and call get_details
 		// check function exist first
 		$path = dirname( __FILE__ ) . '/classes/';
@@ -75,13 +76,11 @@ class AdminMenu extends Boot {
 
 			// test for required functions
 			$class = new ReflectionClass( $class_name );
-			if ( $class->implementsInterface( 'uncanny_learndash_public\RequiredFunctions' ) )
-			{
+			if ( $class->implementsInterface( 'uncanny_learndash_public\RequiredFunctions' ) ) {
 				$details[ $class_name ] = $class_name::get_details();
-			}else{
+			} else {
 				$details[ $class_name ] = false;
 			}
-
 		}
 
 		return $details;
@@ -93,13 +92,13 @@ class AdminMenu extends Boot {
 	 */
 	public static function scripts( $hook ) {
 		if ( 'toplevel_page_uo-menu-slug' != $hook ) {
-		   return;
+			return;
 		}
 
 		// Admin CSS
-		wp_enqueue_style( 'uo-menu-slug-css', Config::get_admin_css('admin-style.css') );
+		wp_enqueue_style( 'uo-menu-slug-css', Config::get_admin_css( 'admin-style.css' ) );
 		// Admin JS
-		wp_enqueue_script( 'uo-menu-slug-js', Config::get_admin_js('script.js'), array( 'jquery' ), false, true );
+		wp_enqueue_script( 'uo-menu-slug-js', Config::get_admin_js( 'script.js' ), array( 'jquery' ), false, true );
 		// Admin CSS
 		wp_enqueue_style( 'uo-menu-slug-css-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 
@@ -119,20 +118,28 @@ class AdminMenu extends Boot {
 		<div class="uo-admin-header">
 
 			<a href="http://www.uncannyowl.com" target="_blank">
-				<img src="<?php echo Config::get_admin_media('Uncanny-Owl-logo.png'); ?>" />
+				<img src="<?php echo esc_url( Config::get_admin_media( 'Uncanny-Owl-logo.png' ) ); ?>" />
 			</a>
 
 			<hr class="uo-underline">
 
-			<h2>Thanks for using the Uncanny LearnDash Toolkit!</h2>
+			<h2><?php _e( 'Thanks for using the Uncanny LearnDash Toolkit!', Config::get_text_domain() ); ?></h2>
 
-			<p>This plugin makes it easier to incorporate some of the most common things we see requested on LearnDash
-			sites. Instructions for each feature are provided below, and we also have a helpful screencasts available at
-			<strong>!!![link]!!!</strong>. Consider turning off any features you don’t need to maximize performance.</p>
+			<p><?php
+			printf(
+				__( 'This plugin makes it easier to incorporate some of the most common things we see requested on LearnDash
+				sites. Instructions for each feature are provided below, and we also have a helpful screencasts available at
+				<strong><a href="%s" target="_blank"></strong>. Consider turning off any features you don’t need to maximize performance.', Config::get_text_domain() ) ,
+			esc_url( 'http://www.uncannyowl.com' ) );
+			?></p>
 
-			<p>This is Uncanny Owl’s first LearnDash plugin that we've published in the WordPress repository. We’ll be
-			releasing more plugins over the next few months as well as adding new features to this plugin. Sign up for
-			our mailing list at <a href="www.uncannyowl.com" target="_blank">Uncanny Owl</a> so you don’t miss any updates!</p>
+			<p><?php
+			printf(
+				__( 'This is Uncanny Owl’s first LearnDash plugin that we\'ve published in the WordPress repository. We’ll be
+				releasing more plugins over the next few months as well as adding new features to this plugin. Sign up for
+				our mailing list at <a href="%s" target="_blank">Uncanny Owl</a> so you don’t miss any updates!', Config::get_text_domain() ) ,
+			esc_url( 'http://www.uncannyowl.com' ) );
+			?></p>
 
 		</div>
 		<form method="post" action="options.php">
@@ -143,9 +150,7 @@ class AdminMenu extends Boot {
 		<div class="uo_feature_container">
 			<?php self::create_features( $classes_available, $active_classes ); ?>
 		</div>
-
 		<?php
-
 		submit_button();
 		echo '</form>';
 
@@ -157,22 +162,24 @@ class AdminMenu extends Boot {
 	 *
 	 * return echoed String
 	 */
-	public static function create_features( $classes_available, $active_classes ){
+	public static function create_features( $classes_available, $active_classes ) {
 
 		/* If Magic Quotes are enable we need to stripslashes from ouw $active classes */
-		if( function_exists( 'get_magic_quotes_gpc' )){
-			if( get_magic_quotes_gpc() ){
+		if ( function_exists( 'get_magic_quotes_gpc' ) ) {
+			if ( get_magic_quotes_gpc() ) {
 				//strip slashes from all keys in array
-				$active_classes = Config::stripslashes_deep($active_classes);
+				$active_classes = Config::stripslashes_deep( $active_classes );
 			}
 		}
 
 		foreach ( $classes_available as $key => $class ) {
-			if( false === $class){
+			if ( false === $class ) {
 				?>
 					<div class="uo_feature">
-						<div class="uo_feature_title"><?php echo $key; ?></div>
-						<div class="uo_feature_description">This class is not configured properly. Contact Support for assistance.</div>
+						<div class="uo_feature_title"><?php echo esc_html( $key ) ?></div>
+						<div class="uo_feature_description"><?php
+							esc_html_e( 'This class is not configured properly. Contact Support for assistance.',  Config::get_text_domain() );
+						?></div>
 					</div>
 				<?php
 				continue;
@@ -182,19 +189,19 @@ class AdminMenu extends Boot {
 
 			$is_activated = 'uo_feature_deactivated';
 			$class_name = $key;
-			if( isset( $active_classes[ $class_name ] ) ){
+			if ( isset( $active_classes[ $class_name ] ) ) {
 				$is_activated = 'uo_feature_activated';
 			}
-			if( true !== $dependants_exist ){
+			if ( true !== $dependants_exist ) {
 				$is_activated = 'uo_feature_needs_dependants';
 			}
 
 			$icon = '<div class="uo_icon"></div>';
-			if( $class['icon'] ){
+			if ( $class['icon'] ) {
 				$icon = $class['icon'];
 			}
 
-			if( false === $class['settings'] || !isset( $class['settings'] ) ){
+			if ( false === $class['settings'] || ! isset( $class['settings'] ) ) {
 				$class['settings']['modal'] = '';
 				$class['settings']['link'] = '';
 			}
@@ -206,7 +213,6 @@ class AdminMenu extends Boot {
 
 			<div class="uo_feature">
 
-
 				<?php // Settings Modal Popup trigger
 				echo $class['settings']['link']; ?>
 
@@ -214,11 +220,13 @@ class AdminMenu extends Boot {
 
 					<?php echo $class['title']; ?>
 
-					<?php // Link to KB for Feature
-						if( null !== $class['kb_link'] ){ ?>
-							<a class="uo_feature_more_info" href="<?php echo $class['kb_link']; ?>" target="_blank">
-								<i class="fa fa-question-circle"></i>
-							</a>
+					<?php
+					 // Link to KB for Feature
+					if ( null !== $class['kb_link'] ) {
+					?>
+						<a class="uo_feature_more_info" href="<?php echo $class['kb_link']; ?>" target="_blank">
+							<i class="fa fa-question-circle"></i>
+						</a>
 					<?php } ?>
 
 				</div>
@@ -228,20 +236,21 @@ class AdminMenu extends Boot {
 				<div class="uo_feature_button <?php echo $is_activated; ?>">
 
 					<?php
-					if( true !== $dependants_exist ){
-					 echo '<div><strong>'. $dependants_exist .'</strong> is needed for this add-on</div>';
-					}else{
+					if ( true !== $dependants_exist ) {
+						echo '<div><strong>'. esc_html( $dependants_exist ) .'</strong>' . __( 'is needed for this add-on', Config::get_text_domain() ) . '</div>';
+					} else {
 						?>
 						<div class="uo_feature_button_toggle"></div>
-						<label class="uo_feature_label" for="<?php echo $class_name; ?>">Activate <?php echo $class['title']; ?></label>
-						<input class="uo_feature_checkbox" type="checkbox" id="<?php echo $class_name; ?>" name="uncanny_public_active_classes[<?php echo  $class_name; ?>]" value="<?php echo  $class_name; ?>" <?php
-						if (  array_key_exists( $class_name,$active_classes ) ) {
+						<label class="uo_feature_label" for="<?php echo esc_attr( $class_name ) ?>">
+							<?php echo ( __( 'Activate ', Config::get_text_domain() ) . $class['title'] ); ?>
+						</label>
+						<input class="uo_feature_checkbox" type="checkbox" id="<?php echo esc_attr( $class_name ); ?>" name="uncanny_public_active_classes[<?php echo esc_attr( $class_name ) ?>]" value="<?php echo esc_attr( $class_name ) ?>" <?php
+						if (  array_key_exists( $class_name, $active_classes ) ) {
 							checked( $active_classes[ $class_name ], $class_name, true );
 						}
 						?>
 						/>
 					<?php } ?>
-
 				</div>
 			</div>
 		<?php
