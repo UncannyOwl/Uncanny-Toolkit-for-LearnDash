2a0ed1c5091f7a760bf275193a882ba36669e846
Ken Young <ken@uncannyowl.com>
2016-03-16 13:49:57 +0000

    version 1.0

--- uncanny-learndash-public.php
+++ uncanny-learndash-public.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Uncanny LearnDash Toolkit
-Version: 0.2.9
+Version: 1.0
 Description: Extend the LearnDash plugin with a variety of complementary features to make building engaging learner experiences even easier.
 Author: www.uncannyowl.com
 Author URI: www.uncannyowl.com
