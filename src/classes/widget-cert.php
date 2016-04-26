33233f41ea3228ad9732e001dd00c51c8fed9d35
Ernest Smuga <info@afiadesign.com>
2016-04-26 12:55:14 -0400

    v1.2.4 - Add message if no certificates available

--- src/classes/widget-cert.php
+++ src/classes/widget-cert.php
@@ -2,7 +2,7 @@
 
 namespace uncanny_learndash_toolkit;
 
-if (!defined('WPINC')) {
+if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
@@ -10,8 +10,7 @@ if (!defined('WPINC')) {
  * Class WidgetCert
  * @package uncanny_custom_toolkit
  */
-class WidgetCert extends \WP_Widget implements RequiredFunctions
-{
+class WidgetCert extends \WP_Widget implements RequiredFunctions {
 	static $instance;
 
 	/**
@@ -19,20 +18,19 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 *
 	 * @return array
 	 */
-	public static function get_details()
-	{
-		$class_title = esc_html__('LearnDash Certificate Widget', 'uncanny-learndash-toolkit');
-		$kb_link = null;
-		$class_description = esc_html__('Custom Widget that displays all certificates the user has earned.', 'uncanny-learndash-toolkit');
-		$class_icon = '<i class="uo_icon_fa fa fa-bookmark "></i>';
+	public static function get_details() {
+		$class_title       = esc_html__( 'LearnDash Certificate Widget', 'uncanny-learndash-toolkit' );
+		$kb_link           = null;
+		$class_description = esc_html__( 'Custom Widget that displays all certificates the user has earned.', 'uncanny-learndash-toolkit' );
+		$class_icon        = '<i class="uo_icon_fa fa fa-bookmark "></i>';
 
 		return array(
-			'title' => $class_title,
-			'kb_link' => $kb_link, // OR set as null not to display
-			'description' => $class_description,
+			'title'            => $class_title,
+			'kb_link'          => $kb_link, // OR set as null not to display
+			'description'      => $class_description,
 			'dependants_exist' => self::dependants_exist(),
-			'settings' => false,
-			'icon' => $class_icon,
+			'settings'         => false,
+			'icon'             => $class_icon,
 		);
 	}
 
@@ -41,10 +39,9 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 *
 	 * return boolean || string TRUE or name of function or plugin that is needed
 	 */
-	public static function dependants_exist()
-	{
+	public static function dependants_exist() {
 		global $learndash_post_types;
-		if (!isset($learndash_post_types)) {
+		if ( ! isset( $learndash_post_types ) ) {
 			return 'Plugin: LearnDash';
 		}
 
@@ -55,23 +52,21 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	/**
 	 * class constructor
 	 */
-	public function __construct()
-	{
+	public function __construct() {
 
 		// load the cert widget
-		add_action('widgets_init', array(__CLASS__, 'register_cert_widget'));
+		add_action( 'widgets_init', array( __CLASS__, 'register_cert_widget' ) );
 
 		parent::__construct(
 			'uncanny_lms_cert_list', // Base ID
-				esc_html__('Certificates - Uncanny Toolkit', 'uncanny-learndash-toolkit'), // Name
-			array('description' => esc_html__('Adds a new certificate widget that outputs links to all LearnDash certificates earned by the user.', 'text_domain')) // Args
+			esc_html__( 'Certificates - Uncanny Toolkit', 'uncanny-learndash-toolkit' ), // Name
+			array( 'description' => esc_html__( 'Adds a new certificate widget that outputs links to all LearnDash certificates earned by the user.', 'text_domain' ) ) // Args
 		);
 	}
 
 	// register LMS cert widget
-	public static function register_cert_widget()
-	{
-		register_widget(__NAMESPACE__ . '\WidgetCert');
+	public static function register_cert_widget() {
+		register_widget( __NAMESPACE__ . '\WidgetCert' );
 	}
 
 	/**
@@ -84,46 +79,45 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 *
 	 * @return null
 	 */
-	public function widget($args, $instance)
-	{
+	public function widget( $args, $instance ) {
 
-		if (!is_user_logged_in()) {
+		if ( ! is_user_logged_in() ) {
 			return '';
 		}
 
 		echo $args['before_widget'];
 
-		if (!empty($instance['title'])) {
-			echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
 
 		/* GET Certificates For Courses*/
 		$args = array(
-			'post_type' => 'sfwd-courses',
-			'posts_per_page' => -1,
-			'post_status' => 'publish',
-			'orderby' => 'title',
-			'order' => 'ASC',
+			'post_type'      => 'sfwd-courses',
+			'posts_per_page' => - 1,
+			'post_status'    => 'publish',
+			'orderby'        => 'title',
+			'order'          => 'ASC',
 		);
 
-		$courses = get_posts($args);
+		$courses = get_posts( $args );
 
-		$certificate_list = '';
+		$certificate_list   = '';
 		$certificate_titles = Array();
 
-		foreach ($courses as $course) {
+		foreach ( $courses as $course ) {
 
-			$certificate_id = learndash_get_setting($course->ID, 'certificate');
-			$certificate_object = get_post($certificate_id);
-			$certificate_title = $certificate_object->post_title;
-			$certificate_link = learndash_get_course_certificate_link($course->ID);
+			$certificate_id     = learndash_get_setting( $course->ID, 'certificate' );
+			$certificate_object = get_post( $certificate_id );
+			$certificate_title  = $certificate_object->post_title;
+			$certificate_link   = learndash_get_course_certificate_link( $course->ID );
 
-			if ($certificate_link && '' !== $certificate_link) {
+			if ( $certificate_link && '' !== $certificate_link ) {
 
-				if ($certificate_link && '' !== $certificate_link) {
-					if (!in_array($certificate_title, $certificate_titles)) {
-						$certificate_list .= '<li><a target="_blank" href="' . $certificate_link . '" title="' . esc_html__('Your certificate for :', 'uncanny-learndash-toolkit' . $course->post_title) . '">' . $certificate_title . '</a></li>';
-						array_push($certificate_titles, $certificate_title);
+				if ( $certificate_link && '' !== $certificate_link ) {
+					if ( ! in_array( $certificate_title, $certificate_titles ) ) {
+						$certificate_list .= '<li><a target="_blank" href="' . $certificate_link . '" title="' . esc_html__( 'Your certificate for :', 'uncanny-learndash-toolkit' . $course->post_title ) . '">' . $certificate_title . '</a></li>';
+						array_push( $certificate_titles, $certificate_title );
 					}
 				}
 
@@ -135,40 +129,51 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 
 		echo '<div class="uncanny-cert-widget-list">';
 
-		if (!empty($quiz_attempts) || '' !== $certificate_list) {
+		if ( ! empty( $quiz_attempts ) || '' !== $certificate_list ) {
 
 			echo '<ul>';
 
 			echo $certificate_list;
 
-			$quiz_attempts = array_reverse($quiz_attempts);
+			$quiz_attempts     = array_reverse( $quiz_attempts );
+			$certificate_count = 0;
 
-			foreach ($quiz_attempts as $k => $quiz_attempt) {
+			foreach ( $quiz_attempts as $k => $quiz_attempt ) {
 
-				$certificateLink = $quiz_attempt['certificate']['certificateLink'];
-				$quiz_title = !empty($quiz_attempt['post']->post_title) ? $quiz_attempt['post']->post_title : @$quiz_attempt['quiz_title'];
+				if ( isset( $quiz_attempt['certificate'] ) ) {
+					$certificate_count ++;
+					$certificateLink = $quiz_attempt['certificate']['certificateLink'];
+					$quiz_title      = ! empty( $quiz_attempt['post']->post_title ) ? $quiz_attempt['post']->post_title : @$quiz_attempt['quiz_title'];
 
-				if (!empty($certificateLink)) {
-					$meta = get_post_meta($quiz_attempt['post']->ID, '_sfwd-quiz', true);
-					$certificate_id = $meta['sfwd-quiz_certificate'];
-					$certificate_object = get_post($certificate_id);
-					$certificate_title = $certificate_object->post_title;
+					if ( ! empty( $certificateLink ) ) {
+						$meta               = get_post_meta( $quiz_attempt['post']->ID, '_sfwd-quiz', true );
+						$certificate_id     = $meta['sfwd-quiz_certificate'];
+						$certificate_object = get_post( $certificate_id );
+						$certificate_title  = $certificate_object->post_title;
 
-					if (!in_array($certificate_title, $certificate_titles)) {
-						printf('<li><a target="_blank" href="%s" title="%s" > %s</a></li>',
-							esc_url($certificateLink),
-							esc_html__('Your certificate for :', 'uncanny-learndash-toolkit' . $quiz_title),
-							esc_html($certificate_title)
-						);
-						array_push($certificate_titles, $certificate_title);
+						if ( ! in_array( $certificate_title, $certificate_titles ) ) {
+							printf( '<li><a target="_blank" href="%s" title="%s" > %s</a></li>',
+								esc_url( $certificateLink ),
+								esc_html__( 'Your certificate for :', 'uncanny-learndash-toolkit' . $quiz_title ),
+								esc_html( $certificate_title )
+							);
+							array_push( $certificate_titles, $certificate_title );
+						}
 					}
 				}
+
+			}
+
+			if ( 0 === $certificate_count ) {
+				printf( '<li> %s</li>',
+						esc_html__( 'Complete courses to earn certificates', 'uncanny-learndash-toolkit' )
+				);
 			}
 
 			echo '</ul></div>';
 
 		} else {
-			printf('<p>%s</p></div>', esc_html($instance['no_certs']));
+			printf( '<p>%s</p></div>', esc_html( $instance['no_certs'] ) );
 		}
 
 		//echo $args['after_widget'];
@@ -180,28 +185,27 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 * modified from code in wp-content/plugins/sfwd-lms/course_info_widget.php
 	 * @return array
 	 */
-	private static function quiz_attempts()
-	{
+	private static function quiz_attempts() {
 
 		$quiz_attempts = array();
-		$current_user = wp_get_current_user();
+		$current_user  = wp_get_current_user();
 
-		if (empty($current_user->ID)) {
+		if ( empty( $current_user->ID ) ) {
 			return $quiz_attempts;
 		}
 
-		$user_id = $current_user->ID;
-		$quiz_attempts_meta = get_user_meta($user_id, '_sfwd-quizzes', true);
-		if (!(empty($quiz_attempts_meta) || false === $quiz_attempts_meta)) {
-			foreach ($quiz_attempts_meta as $quiz_attempt) {
-				$quiz_attempt['post'] = get_post($quiz_attempt['quiz']);
-				$c = learndash_certificate_details($quiz_attempt['quiz'], $user_id);
+		$user_id            = $current_user->ID;
+		$quiz_attempts_meta = get_user_meta( $user_id, '_sfwd-quizzes', true );
+		if ( ! ( empty( $quiz_attempts_meta ) || false === $quiz_attempts_meta ) ) {
+			foreach ( $quiz_attempts_meta as $quiz_attempt ) {
+				$quiz_attempt['post'] = get_post( $quiz_attempt['quiz'] );
+				$c                    = learndash_certificate_details( $quiz_attempt['quiz'], $user_id );
 				if (
 					get_current_user_id() === $user_id &&
-					!empty($c['certificateLink']) &&
+					! empty( $c['certificateLink'] ) &&
 					(
-					(isset($quiz_attempt['percentage']) &&
-						$quiz_attempt['percentage'] >= $c['certificate_threshold'] * 100
+					( isset( $quiz_attempt['percentage'] ) &&
+					  $quiz_attempt['percentage'] >= $c['certificate_threshold'] * 100
 					)
 					)
 				) {
@@ -224,24 +228,23 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 *
 	 * @return string|void
 	 */
-	public function form($instance)
-	{
-		$title = !empty($instance['title']) ? $instance['title'] : esc_html__('Your certificates', 'uncanny-learndash-toolkit');
-		$no_certs = !empty($instance['no_certs']) ? $instance['no_certs'] : esc_html__('Complete courses to earn certificates', 'uncanny-learndash-toolkit');
+	public function form( $instance ) {
+		$title    = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Your certificates', 'uncanny-learndash-toolkit' );
+		$no_certs = ! empty( $instance['no_certs'] ) ? $instance['no_certs'] : esc_html__( 'Complete courses to earn certificates', 'uncanny-learndash-toolkit' );
 
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
-				   name="<?php echo $this->get_field_name('title'); ?>" type="text"
-				   value="<?php echo esc_attr($title); ?>">
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
+			       value="<?php echo esc_attr( $title ); ?>">
 		</p>
 		<p>
 			<label
-				for="<?php echo $this->get_field_id('no_certs'); ?>"><?php esc_html_e('No certificates message:', 'uncanny-learndash-toolkit'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('no_certs'); ?>"
-				   name="<?php echo $this->get_field_name('no_certs'); ?>" type="text"
-				   value="<?php echo esc_attr($no_certs); ?>">
+				for="<?php echo $this->get_field_id( 'no_certs' ); ?>"><?php esc_html_e( 'No certificates message:', 'uncanny-learndash-toolkit' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'no_certs' ); ?>"
+			       name="<?php echo $this->get_field_name( 'no_certs' ); ?>" type="text"
+			       value="<?php echo esc_attr( $no_certs ); ?>">
 		</p>
 
 		<?php
@@ -257,11 +260,10 @@ class WidgetCert extends \WP_Widget implements RequiredFunctions
 	 *
 	 * @return array Updated safe values to be saved.
 	 */
-	public function update($new_instance, $old_instance)
-	{
-		$instance = array();
-		$instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
-		$instance['no_certs'] = (!empty($new_instance['no_certs'])) ? strip_tags($new_instance['no_certs']) : '';
+	public function update( $new_instance, $old_instance ) {
+		$instance             = array();
+		$instance['title']    = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['no_certs'] = ( ! empty( $new_instance['no_certs'] ) ) ? strip_tags( $new_instance['no_certs'] ) : '';
 
 		return $instance;
 	}
