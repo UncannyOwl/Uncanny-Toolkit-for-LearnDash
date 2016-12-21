a058d342f8b48809637262f6761264aed4fce005
Ernest <ernest@uncannyowl.com>
2016-12-21 07:50:36 -0800

    release changes: versions, changelog

--- src/config.php
+++ src/config.php
@@ -278,7 +278,7 @@ class Config {
 	 */
 	public static function get_version() {
 		if ( null === self::$version ) {
-			self::$version = '1.3.4';
+			self::$version = '1.3.5';
 		}
 
 		return self::$version;
