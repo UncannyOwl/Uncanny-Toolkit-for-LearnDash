9db51badcd8536152a7bf14b7c7cd7e23e48481c
Ahsan Cheema <ahsancheema26@gmail.com>
2018-11-09 01:46:16 +0500

    3.0 RESET Password fix in front-end login module

--- src/templates/login-page-ui.php
+++ src/templates/login-page-ui.php
@@ -401,6 +401,50 @@ $innerText = apply_filters( 'uo-login-inner-text', $innerText, $login );
 			if ( isset( $_POST['pass1'] ) && $_POST['pass1'] != $_POST['pass2'] ) {
 
 				echo '<h2>' . $innerText['Password-Not-Match'] . '</h2>';
+				$rp_key    = $_POST['rp_key'];
+				$rp_login  = $_POST['rp_login'];
+				$rp_cookie = 'wp-resetpass-' . COOKIEHASH;
+				$value     = sprintf( '%s:%s', wp_unslash( $rp_key ), wp_unslash( $rp_login ) );
+				//setcookie( $rp_cookie, $value, 0, '/' . get_post_field( 'post_name', $login_page ), COOKIE_DOMAIN, is_ssl(), true );
+				
+				?>
+                <h2><?php echo $innerText['Reset-Password-Title']; ?></h2>
+                <form name="resetpassform" id="resetpassform"
+                      action="?action=validatepasswordreset" method="post"
+                      autocomplete="off">
+                    <input type="hidden" id="user_login" name="rp_login" value="<?php echo esc_attr( $rp_login ); ?>"
+                           autocomplete="off"/>
+
+                    <div class="user-pass1-wrap">
+                        <p>
+                            <label for="pass1"><?php echo $innerText['New-Password']; ?></label>
+                        </p>
+
+                        <div class="wp-pwd">
+                            <span class="password-input-wrapper">
+                                <input type="password" data-reveal="1"
+                                       data-pw="<?php echo esc_attr( wp_generate_password( 16 ) ); ?>" name="pass1"
+                                       id="pass1" class="input" size="20" value="" autocomplete="off"
+                                       aria-describedby="pass-strength-result"/>
+                            </span>
+                        </div>
+                    </div>
+                    <p class="user-pass2-wrap">
+                        <label for="pass2"><?php echo $innerText['Confirm-Password']; ?></label><br/>
+                        <input type="password" name="pass2" id="pass2" class="input" size="20" value="" autocomplete="off"/>
+                    </p>
+
+                    <p class="description indicator-hint"><?php echo $innerText['Password-Indicator-Hint']; ?></p>
+                    <br class="clear"/>
+                    <input type="hidden" name="rp_key" value="<?php echo esc_attr( $rp_key ); ?>"/>
+					<?php if( '' !== trim($recaptcha_key)){?>
+                        <div class="g-recaptcha" data-sitekey="<?php echo $recaptcha_key;?>" data-callback="correctCaptcha" data-expired-callback="expiredCaptcha"></div>
+					<?php }?>
+                    <p class="submit"><input type="submit" name="wp-submit" id="wp-submit"
+                                             class="button button-primary button-large"
+                                             value="<?php esc_attr_e( 'Reset Password' ); ?>"/></p>
+                </form>
+                <?php
 
 			} elseif ( isset( $_POST['pass1'] ) && ! empty( $_POST['pass1'] ) ) {
 
