29ab82a638a29df7d11edb9aae0b467cb172e662
Agustin Barrientos <agustin@uncannycloud.com>
2018-12-19 23:28:02 -0300

    3.0 Add the description field to the default template

--- src/templates/frontend-login/default-login.php
+++ src/templates/frontend-login/default-login.php
@@ -21,12 +21,22 @@ echo $default_js;
 
 <?php
 
-$user_name_label = Config::get_settings_value( 'uo_login_username_label', 'FrontendLoginPlus' );
-$show_label = Config::get_settings_value( 'uo_frontendloginplus_hide_title_label', 'FrontendLoginPlus' );
-$login_form_args = apply_filters( 'uo_frontend_login_args', FrontendLoginPlus::fetch_login_form_args( $user_name_label ) );
+$user_name_label   = Config::get_settings_value( 'uo_login_username_label', 'FrontendLoginPlus' );
+$show_label        = Config::get_settings_value( 'uo_frontendloginplus_hide_title_label', 'FrontendLoginPlus' );
+$login_form_args   = apply_filters( 'uo_frontend_login_args', FrontendLoginPlus::fetch_login_form_args( $user_name_label ) );
+$login_description = Config::get_settings_value( 'uo_frontend_login_description', 'FrontendLoginPlus', esc_html__( 'Login to your account to access your courses.', 'uncanny-learndash-toolkit' ) );
 
 if ( 'on' !== $show_label ) { ?>
 	<h2><?php echo $innerText['Login-Title']; ?></h2>
+
+	<?php if ( ! empty( $login_description ) ){ ?>
+		
+		<p class="uo-login-description">
+			<?php echo $login_description; ?>
+		</p>
+
+	<?php } ?>
+
 	<?php
 }
 wp_login_form( $login_form_args );
