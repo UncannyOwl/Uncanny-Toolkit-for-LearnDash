7873ecc2b34aa5f211f5ec14403112def927d08f
Ernest <ernest@uncannyowl.com>
2019-01-07 14:36:32 -0800

    3.0 - license activation tab Redirect if Pro < 3.0

--- src/templates/admin-tabs.php
+++ src/templates/admin-tabs.php
@@ -7,17 +7,44 @@ if ( isset( $_GET['page'] ) ) {
 }
 ?>
 <h2 class="nav-tab-wrapper">
-	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit' ) ?>" class="nav-tab <?php echo 'uncanny-toolkit' === $active_tab ? 'nav-tab-active' : ''; ?>">
+	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit' ) ?>"
+	   class="nav-tab <?php echo 'uncanny-toolkit' === $active_tab ? 'nav-tab-active' : ''; ?>">
 		<?php _e( 'Modules', 'uncanny-learndash-toolkit' ) ?>
 	</a>
-	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-kb' ) ?>" class="nav-tab <?php echo 'uncanny-toolkit-kb' === $active_tab ? 'nav-tab-active' : ''; ?>">
+	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-kb' ) ?>"
+	   class="nav-tab <?php echo 'uncanny-toolkit-kb' === $active_tab ? 'nav-tab-active' : ''; ?>">
 		<?php _e( 'Help', 'uncanny-learndash-toolkit' ) ?>
 	</a>
-	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-plugins' ) ?>" class="nav-tab <?php echo 'uncanny-toolkit-plugins' === $active_tab ? 'nav-tab-active' : ''; ?>">
+	<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-plugins' ) ?>"
+	   class="nav-tab <?php echo 'uncanny-toolkit-plugins' === $active_tab ? 'nav-tab-active' : ''; ?>">
 		<?php _e( 'LearnDash Plugins', 'uncanny-learndash-toolkit' ) ?>
 	</a>
 	<?php if ( Boot::is_pro_active() ) { ?>
-		<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-license' ) ?>" class="nav-tab <?php echo 'uncanny-toolkit-license' === $active_tab ? 'nav-tab-active' : ''; ?>">
+
+		<?php
+		if ( is_defined( 'UNCANNY_TOOLKIT_PRO_VERSION' ) ) {
+			$compare_version = version_compare( UNCANNY_TOOLKIT_PRO_VERSION, '3.0' );
+
+			if ( 0 >= $compare_version ) {
+				?>
+				<a href="<?php echo admin_url( 'admin.php?page=uncanny-pro-license-activation' ) ?>"
+				   class="nav-tab <?php echo 'uncanny-toolkit-license' === $active_tab ? 'nav-tab-active' : ''; ?>">
+					<?php _e( 'License Activation', 'uncanny-learndash-toolkit' ) ?>
+				</a>
+				<?php
+			}else{
+				?>
+				<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-license' ) ?>"
+				   class="nav-tab <?php echo 'uncanny-toolkit-license' === $active_tab ? 'nav-tab-active' : ''; ?>">
+					<?php _e( 'License Activation', 'uncanny-learndash-toolkit' ) ?>
+				</a>
+				<?php
+			}
+
+		}
+		?>
+		<a href="<?php echo admin_url( 'admin.php?page=uncanny-toolkit-license' ) ?>"
+		   class="nav-tab <?php echo 'uncanny-toolkit-license' === $active_tab ? 'nav-tab-active' : ''; ?>">
 			<?php _e( 'License Activation', 'uncanny-learndash-toolkit' ) ?>
 		</a>
 	<?php }
