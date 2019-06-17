e6ee75e1a6831179c69570936cc324e2fb489696
Ernest <ernest@uncannyowl.com>
2019-06-17 11:17:31 -0700

    Revert "fix paragraphs not formating in tinymce text area for modules"
    This reverts commit 31eb0c40a2a72cdb9500393ea219682c7cc43135.

--- src/assets/backend/js/functions.js
+++ src/assets/backend/js/functions.js
@@ -619,11 +619,7 @@ jQuery( function($){
                                 let editor = tinymce.get( field.name );
 
                                 if ( ULT_Utility.isDefined( editor ) ){
-                                    if( typeof wp.editor !== 'undefined' && typeof wp.editor.autop !== 'undefined'  ){
-                                        editor.setContent( wp.editor.autop( field.value ) );
-                                    }else{
-                                        editor.setContent( field.value );
-                                    }
+                                    editor.setContent( field.value );
                                 }
                             }
                             break;
