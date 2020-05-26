fb25dfc7ae8c48aa20767b53b87a7f2fc46bc874
Agustin Barrientos <agustin@uncannyowl.com>
2020-05-26 09:28:39 -0300

    3.3 Reviews banner - Change text - Change text domain

--- src/templates/admin-review-banner.php
+++ src/templates/admin-review-banner.php
@@ -3,17 +3,19 @@
 	<div class="ult">
 		<div class="ult-review-banner">
 			<div class="ult-review-banner__title">
-				<?php _ex( 'Enjoying Uncanny LearnDash Toolkit?', 'Reviews banner', 'uncanny-automator' ); ?> 🙂
+				<?php printf( 
+					/* translators: 1. Trademarked term */
+					_x( 'Has the %1$s been useful for your site?', 'Reviews banner', 'uncanny-learndash-toolkit' ), 'Uncanny Toolkit for LearnDash' ); ?> 🙂
 			</div>
 			<div class="ult-review-banner__description">
-				<?php _ex( '5-star reviews inspire us to make the plugin even better!', 'Reviews banner', 'uncanny-automator' ); ?>
+				<?php _ex( '5-star reviews inspire us to make the plugin even better!', 'Reviews banner', 'uncanny-learndash-toolkit' ); ?>
 			</div>
 			<div class="ult-review-banner__actions">
 				<a href="<?php echo $url_send_review; ?>" target="_blank" class="ult-review-banner__action ult-review-banner__action--primary" data-action="hide-forever">
 					<?php
 
 					/* translators: Non-personal infinitive verb */
-					_ex( 'Add my review', 'Reviews banner', 'uncanny-automator' );
+					_ex( 'Add my review', 'Reviews banner', 'uncanny-learndash-toolkit' );
 
 					?>
 				</a>
@@ -22,21 +24,21 @@
 					<?php
 
 					/* translators: Non-personal infinitive verb */
-					_ex( 'Send feedback', 'Reviews banner', 'uncanny-automator' );
+					_ex( 'Send feedback', 'Reviews banner', 'uncanny-learndash-toolkit' );
 
 					?>
 				</a>
 				
 				<div class="ult-review-banner__action ult-review-banner__action--secondary" data-action="maybe-later">
-					<?php _ex( 'Maybe later', 'Reviews banner', 'uncanny-automator' ); ?>
+					<?php _ex( 'Maybe later', 'Reviews banner', 'uncanny-learndash-toolkit' ); ?>
 				</div>
 				
 				<div class="ult-review-banner__action ult-review-banner__action--anchor ult-review-banner__action--no-margin-right" data-action="hide-forever">
-					<span class="ult-review-banner__action--anchor-border"><?php _ex( 'No, thanks', 'Reviews banner', 'uncanny-automator' ); ?></span>
+					<span class="ult-review-banner__action--anchor-border"><?php _ex( 'No, thanks', 'Reviews banner', 'uncanny-learndash-toolkit' ); ?></span>
 				</div>
 			</div>
 			<div id="ult-review-banner__close">
-				<div class="ult-review-banner__close-tooltip" ult-tooltip-admin="<?php _ex( 'Maybe later', 'Reviews banner', 'uncanny-automator' ); ?>" ult-flow-admin="left"></div>
+				<div class="ult-review-banner__close-tooltip" ult-tooltip-admin="<?php _ex( 'Maybe later', 'Reviews banner', 'uncanny-learndash-toolkit' ); ?>" ult-flow-admin="left"></div>
 				<div class="ult-review-banner__close-icon"></div>
 			</div>
 		</div>
